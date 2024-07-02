cffcc9256128b370840cda1097c418b32f173631
DmitriyBabii <v.babiy75@gmail.com>
2024-07-02 13:29:50 +0300

    add init files

---
+++ src/test/java/ua/without_jpa/WithoutJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.without_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithoutJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
